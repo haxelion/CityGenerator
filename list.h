1e7fe0af8875e01eba3add7448a1ba0cd9196c13
haxelion <haxelion@gmail.com>
2012-04-21 13:22:47 +0200

    Mise en inline des fonctions de de la classe List

--- list.h
+++ list.h
@@ -28,158 +28,154 @@ private:
     ListItem<T> *first, *current;
     int size, position;
 
-    void next();
+    void next()
+    {
+        if(position+1<size)
+        {
+            current = current->getNext();
+            position++;
+        }
+    }
 
 public:
 
 
-    List();
-    ~List();
-    void addElement(T a);
-    void removeElementAt(int position);
-    void insertElementAt(T a, int position);
-    void setElementAt(T a, int position);
-    void goTo(int position);
-    void start();
-    T getElementAt(int position);
-    T getCurrentElement() const {return current;}
-    T getNextElement();
-    int getSize() const {return size;}
-    bool isAtTheEnd() const {return position+1 == size;}
+    List()
+    {
+        first = 0;
+        current = 0;
+        size = 0;
+        position = 0;
+    }
+
+    ~List()
+    {
+        //on revient au début de la liste
+        start();
+        ListItem<T> *t;
+        //on parcourt toute la chaine en supprimant les éléments un à un
+        while(position<size)
+        {
+            t = current;
+            current = current->getNext();
+            delete t;
+            position++;
+        }
+    }
+
+    void addElement(T a)
+    {
+        //insertion de a au début de la liste
+        ListItem<T> *t = first;
+        first = new ListItem<T>(a);
+        //lien du reste de la liste au nouveau premier maillon
+        first->setNext(t);
+        //mise à jour de la taille et de la position de current
+        size++;
+        position++;
+    }
+
+    void removeElementAt(int position)
+    {
+        if(position == 0)
+        {
+            ListItem<T> *t = first->getNext();
+            delete first;
+            first = t;
+        }
+        else
+        {
+            //on veut trouver l'élément juste avant celui à supprimer
+            goTo(position--);
+            //on mémorise l'élément après celui à supprimer
+            ListItem<T> *t = (current->getNext())->getNext();
+            //on supprime l'élément
+            delete current->getNext();
+            //on lie l'élément avant avec l'élément après pour reformer la liste
+            current->setNext(t);
+        }
+        //on met à jour la taille
+        size--;
+
+    }
+
+    void insertElementAt(T a, int position)
+    {
+        if(position == 0)
+        {
+            addElement(a);
+        }
+        else
+        {
+            //on veut trouver l'élément juste avant l'endroit d'insertion
+            goTo(position--);
+            //on mémorise l'élément à la position d'insertion
+            ListItem<T> *t = current->getNext();
+            //on insère l'élément
+            current->setNext(new ListItem<T>(a));
+            //on reforme la liste
+            (current->getNext())->setNext(t);
+
+        }
+        //mise à jour de la taille et de la position de current
+        size++;
+        position++;
+    }
+
+    void setElementAt(T a, int position)
+    {
+        goTo(position);
+        current->setElement(a);
+    }
+
+    void goTo(int position)
+    {
+        //si la position est en dehors de la liste, on la met au borne
+        if(position<0)
+            position = 0;
+        else if(position>=size)
+            position = size-1;
+        //si cet élément se trouve avant current, il faut reparcourir la liste depuis le début
+        if(position<this->position)
+            start();
+        //on parcourt la liste jusqu'à trouver cet élément
+        while(this->position != position)
+            next();
+    }
+
+    void start()
+    {
+        current = first;
+        this->position = 0;
+    }
+
+    T getElementAt(int position)
+    {
+        goTo(position);
+        return current->getElement();
+    }
+
+    T getCurrentElement() const
+    {
+        return current;
+    }
+
+    T getNextElement()
+    {
+        //on ne dépasse pas la fin de la liste
+        if(position+1<size)
+            next();
+        return current->getElement();
+    }
+    int getSize() const
+    {
+        return size;
+    }
+
+    bool isAtTheEnd() const
+    {
+        return position+1 == size;
+    }
 
 };
-
-template <class T> List<T>::List()
-{
-    first = 0;
-    current = 0;
-    size = 0;
-    position = 0;
-}
-
-template <class T> List<T>::~List()
-{
-    //on revient au début de la liste
-    start();
-    ListItem<T> *t;
-    //on parcourt toute la chaine en supprimant les éléments un à un
-    while(position<size)
-    {
-        t = current;
-        current = current->getNext();
-        delete t;
-        position++;
-    }
-}
-
-template <class T> void List<T>::addElement(T a)
-{
-    //insertion de a au début de la liste
-    ListItem<T> *t = first;
-    first = new ListItem<T>(a);
-    //lien du reste de la liste au nouveau premier maillon
-    first->setNext(t);
-    //mise à jour de la taille et de la position de current
-    size++;
-    position++;
-}
-
-template <class T> void List<T>::goTo(int position)
-{
-    //si la position est en dehors de la liste, on la met au borne
-    if(position<0)
-        position = 0;
-    else if(position>=size)
-        position = size-1;
-    //si cet élément se trouve avant current, il faut reparcourir la liste depuis le début
-    if(position<this->position)
-        start();
-    //on parcourt la liste jusqu'à trouver cet élément
-    while(this->position != position)
-        next();
-}
-
-template <class T> void List<T>::removeElementAt(int position)
-{
-    if(position == 0)
-    {
-        ListItem<T> *t = first->getNext();
-        delete first;
-        first = t;
-    }
-    else
-    {
-        //on veut trouver l'élément juste avant celui à supprimer
-        goTo(position--);
-        //on mémorise l'élément après celui à supprimer
-        ListItem<T> *t = (current->getNext())->getNext();
-        //on supprime l'élément
-        delete current->getNext();
-        //on lie l'élément avant avec l'élément après pour reformer la liste
-        current->setNext(t);
-    }
-    //on met à jour la taille
-    size--;
-
-}
-
-template <class T> void List<T>::insertElementAt(T a, int position)
-{
-    if(position == 0)
-    {
-        addElement(a);
-    }
-    else
-    {
-        //on veut trouver l'élément juste avant l'endroit d'insertion
-        goTo(position--);
-        //on mémorise l'élément à la position d'insertion
-        ListItem<T> *t = current->getNext();
-        //on insère l'élément
-        current->setNext(new ListItem<T>(a));
-        //on reforme la liste
-        (current->getNext())->setNext(t);
-
-    }
-    //mise à jour de la taille et de la position de current
-    size++;
-    position++;
-}
-
-template <class T> void List<T>::next()
-{
-    if(position+1<size)
-    {
-        current = current->getNext();
-        position++;
-    }
-}
-
-template <class T> void List<T>::start()
-{
-    current = first;
-    this->position = 0;
-}
-
-template <class T> void List<T>::setElementAt(T a,int position)
-{
-    goTo(position);
-    current->setElement(a);
-
-}
-
-template <class T> T List<T>::getElementAt(int position)
-{
-    goTo(position);
-    return current->getElement();
-}
-
-template <class T> T List<T>::getNextElement()
-{
-    //on ne dépasse pas la fin de la liste
-    if(position+1<size)
-        next();
-    return current->getElement();
-}
-
 #endif // LIST_H
