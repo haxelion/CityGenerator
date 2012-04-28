66e8d1a870e01ab74a6474cf2135f361fa99b555
unknown <William-James@Kettlewell_PC.(none)>
2012-04-28 18:51:26 +0200

    Début de la fonction de génération des VBO/IBO/VBA pour les jardins et batiments

--- buffers.cpp
+++ buffers.cpp
@@ -4,36 +4,36 @@ Buffers::Buffers()
 {
 }
 
-void Buffers::generateRoadBuffer(List<Element> *roadList)
+void Buffers::generateRoadBuffer(List<Element*> *roadList)
 {
     float *vertex = new float [5*4*roadList->getSize()];
     float *indice = new float [6*roadList->getSize()];
     roadList->start();
-    for(int i=0;!roadList->isAtTheEnd(); i++)
+    for(int i=0;!roadList->isAtTheEnd(); i+=20)
     {
-        // Pour le VBO
-        vertex[i] = roadList->getCurrentElement().getX1();
-        vertex[i+1] = roadList->getCurrentElement().getY1();
+        // Pour le VBO des routes
+        vertex[i] = roadList->getCurrentElement()->getX1();
+        vertex[i+1] = roadList->getCurrentElement()->getY1();
         vertex[i+2] = 0;
         vertex[i+3] = 0;
         vertex[i+4] = 0;
-        vertex[i+5] = roadList->getCurrentElement().getX1();
-        vertex[i+6] = roadList->getCurrentElement().getY2();
+        vertex[i+5] = roadList->getCurrentElement()->getX1();
+        vertex[i+6] = roadList->getCurrentElement()->getY2();
         vertex[i+7] = 0;
         vertex[i+8] = 0;
         vertex[i+9] = 1;
-        vertex[i+10] = roadList->getCurrentElement().getX2();
-        vertex[i+11] = roadList->getCurrentElement().getY2();
+        vertex[i+10] = roadList->getCurrentElement()->getX2();
+        vertex[i+11] = roadList->getCurrentElement()->getY2();
         vertex[i+12] = 0;
         vertex[i+13] = 1;
         vertex[i+14] = 1;
-        vertex[i+15] = roadList->getCurrentElement().getX2();
-        vertex[i+16] = roadList->getCurrentElement().getY1();
+        vertex[i+15] = roadList->getCurrentElement()->getX2();
+        vertex[i+16] = roadList->getCurrentElement()->getY1();
         vertex[i+17] = 0;
         vertex[i+18] = 1;
         vertex[i+19] = 0;
 
-        //Pour le IBO
+        //Pour le IBO des routes
         indice[i]= i;
         indice[i+1]= i+1;
         indice[i+2]= i+2;
@@ -46,11 +46,81 @@ void Buffers::generateRoadBuffer(List<Element> *roadList)
 
 }
 
-void Buffers::generateElementBuffer(List<Element> *BlockList)
+void Buffers::generateElementBuffer(List<Block*> *BlockList)
 {
-    /*for(int i=0;!roadList->isAtTheEnd(); i++)
+    int nombreJardin = 0;
+    int nombreBuilding = 0;
+    blockList->start();
+    while (!BlockList->isAtTheEnd())
     {
-        for (int j = 0; !)
+        List<Element*> *l = BlockList->getCurrentElement()->getElementList();
+        while(!l->isAtTheEnd())
+        {
+            if(l->getCurrentElement()->getType() == BUILDING)
+                nombreBuilding++;
+            if(l->getCurrentElement()->getType()== GARDEN)
+                nombreJardin++;
+            l->next();
+        }
+        BlockList->next();
     }
-    */
+    float *vertexJardin = new float [5*4*nombreJardin];
+    float *indiceJardin = new float [6*nombreJardin];
+    float *vertexBuilding = new float [16*5*nombreBuilding];
+    float *indiceBuilding = new float [4*6*nombreBuilding];
+    float *vertexRoof = new float [4*5*nombreBuilding];
+    float *indiceRoof = new float [6*nombreBuilding];
+
+
+    while (!BlockList->isAtTheEnd())
+    {
+        List<Element*> *l = BlockList->getCurrentElement()->getElementList();
+        int i=0; // Pour les jardins
+        int j=0; // Pour les buildings
+        while (!l->isAtTheEnd())
+        {
+            if(l->getCurrentElement()->getType() == BUILDING)
+            {
+
+            }
+            if(l->getCurrentElement()->getType()== GARDEN)
+            {
+                // Pour le VBO des routes
+                vertex[i] = l->getCurrentElement()->getX1();
+                vertex[i+1] = l->getCurrentElement()->getY1();
+                vertex[i+2] = 0;
+                vertex[i+3] = 0;
+                vertex[i+4] = 0;
+                vertex[i+5] = l->getCurrentElement()->getX1();
+                vertex[i+6] = l->getCurrentElement()->getY2();
+                vertex[i+7] = 0;
+                vertex[i+8] = 0;
+                vertex[i+9] = 1;
+                vertex[i+10] = l->getCurrentElement()->getX2();
+                vertex[i+11] = l->getCurrentElement()->getY2();
+                vertex[i+12] = 0;
+                vertex[i+13] = 1;
+                vertex[i+14] = 1;
+                vertex[i+15] = l->getCurrentElement()->getX2();
+                vertex[i+16] = l->getCurrentElement()->getY1();
+                vertex[i+17] = 0;
+                vertex[i+18] = 1;
+                vertex[i+19] = 0;
+
+                //Pour le IBO des routes
+                indice[i]= i;
+                indice[i+1]= i+1;
+                indice[i+2]= i+2;
+                indice[i+3]= i;
+                indice[i+4]= i+2;
+                indice[i+5]=i+3;
+
+                i +=20;
+            }
+            l->next();
+        }
+        BlockList->next();
+    }
+
+
 }
